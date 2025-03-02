617933152f9d26ed4b8f49d11b31205e53dfb73a
Jeisson177 <79467624+Jeisson177@users.noreply.github.com>
2025-03-02 14:33:20 -0500

    configuracion db

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -1,17 +1,27 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.service.ProductoService;
+
 @Controller
 @RequestMapping("/menu")
 public class ProductoController {
-
+    @Autowired
+    ProductoService productoService;
     // Carga la página HTML de menú
     @GetMapping
     public String mostrarMenu(Model model) {
         return "menu"; // Carga el archivo menu.html desde templates
+    }
+    @GetMapping("/producto")
+    public String mostrarProductos(Model model){
+        model.addAttribute("productos", productoService.getAllProductos());
+        return "menuTabla";
+    
     }
 }
