57d0669c15874764ddfc795e0d99a888f5b96a23
Jeisson177 <79467624+Jeisson177@users.noreply.github.com>
2025-05-13 14:36:31 -0500

    push

--- src/main/java/com/example/demo/controller/ProductoTablaController.java
+++ src/main/java/com/example/demo/controller/ProductoTablaController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,6 +51,7 @@ public class ProductoTablaController {
     // Crear nuevo producto
     @PostMapping
     public ResponseEntity<Producto> guardarProducto(@RequestBody Producto producto) {
+        producto.setTemporada(true);
         Producto nuevoProducto = productoService.guardarProducto(producto);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevoProducto);
     }
