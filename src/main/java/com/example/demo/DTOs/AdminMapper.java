224b9beac1b53c639bb0bda7167d19925b920221
Mariana Perez Casallas <95313566+mperez1516@users.noreply.github.com>
2025-05-25 15:45:48 -0500

    .

--- src/main/java/com/example/demo/DTOs/AdminMapper.java
+++ src/main/java/com/example/demo/DTOs/AdminMapper.java
@@ -1,9 +1,14 @@
 package com.example.demo.DTOs;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.example.demo.entity.Administrador;
 
 @Mapper
 public class AdminMapper {
-   
+    AdminMapper INSTANCE = Mappers.getMapper(AdminMapper.class);
+
+    AdminDTO convert(Administrador admin);
     
 }
