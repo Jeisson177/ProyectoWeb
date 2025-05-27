1afc119917d44c86bedffddb9dd9c9b42933cc84
Mariana Perez Casallas <95313566+mperez1516@users.noreply.github.com>
2025-05-26 23:07:45 -0500

    .

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -1,8 +1,14 @@
 package com.example.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,7 +25,10 @@ public class UserEntity {
     Long id;
 
     private String username;
-    
+
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Role> roles = new ArrayList<>();
     
 }
